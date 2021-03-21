a8abf08dc1802e2abcd7b87e35090528dd17b84b
Ancientkingg <samuelmbruin@yahoo.com>
2021-03-21 19:11:36 +0100

    luminescence now supports different sized atlases! (Thanks Cituation!)🥥

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ if (picture) {
           });
         } else {
           shaderType((shaderType) => {
-            let fsh = `#version 150\n#moj_import <fog.glsl>\nuniform sampler2D Sampler0;uniform vec4 ColorModulator;uniform float FogStart;uniform float FogEnd;uniform vec4 FogColor;in float vertexDistance;in vec4 vertexColor;in vec2 texCoord0;in vec4 normal;out vec4 fragColor;void main() {int x = int(texCoord0.x * 1024);int y = int(texCoord0.y * 1024);vec4 vtc = vertexColor;${content}vec4 color = texture(Sampler0, texCoord0) * vtc * ColorModulator;fragColor = linear_fog(color, vertexDistance, FogStart, FogEnd, FogColor);}`;
+            let fsh = `#version 150\n#moj_import <fog.glsl>\nuniform sampler2D Sampler0;uniform vec4 ColorModulator;uniform float FogStart;uniform float FogEnd;uniform vec4 FogColor;in float vertexDistance;in vec4 vertexColor;in vec2 texCoord0;in vec4 normal;out vec4 fragColor;void main() {int x = int(texCoord0.x * ${dimensions.width});int y = int(texCoord0.y * ${dimensions.height});vec4 vtc = vertexColor;${content}vec4 color = texture(Sampler0, texCoord0) * vtc * ColorModulator;fragColor = linear_fog(color, vertexDistance, FogStart, FogEnd, FogColor);}`;
             let packmcmeta =
               '{"pack":{ "pack_format": 7, "description": "§cEmissive Textures by Ancientkingg"}}';
             fs.mkdir(
